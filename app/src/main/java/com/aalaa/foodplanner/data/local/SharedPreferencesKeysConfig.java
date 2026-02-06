55d70d5d833d6f2dd770ded934cbc6a1c4e1cf7e
Aalaa Adel <aalaaadel02@gmail.com>
2026-02-06 07:36:04 +0200

    Implement SharedPreferences helper and keys configuration for local storage

---
+++ app/src/main/java/com/aalaa/foodplanner/data/local/SharedPreferencesKeysConfig.java
@@ -0,0 +1,10 @@
+package com.aalaa.foodplanner.data.local;
+
+public abstract class SharedPreferencesKeysConfig {
+    public static final String PREF_NAME = "foodplanner_prefs";
+    public static final String KEY_ONBOARDING_COMPLETED = "onboarding_completed";
+    public static final String KEY_LOGGED_IN = "logged_in";
+    public static final String KEY_USER_ID = "user_id";
+    public static final String KEY_DARK_MODE = "dark_mode";
+    public static final String KEY_LANGUAGE = "language";
+}
