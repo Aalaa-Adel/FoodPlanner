3ee2da83751a86ef893c817f79688c3d395e4492
Aalaa Adel <aalaaadel02@gmail.com>
2026-02-06 07:36:19 +0200

    Add data models for filtering meals feature

---
+++ app/src/main/java/com/aalaa/foodplanner/data/model/FilterMeal.java
@@ -0,0 +1,31 @@
+package com.aalaa.foodplanner.data.model;
+public class FilterMeal {
+    private String strMeal;
+    private String strMealThumb;
+    private String idMeal;
+    public String getStrMeal() {
+        return strMeal;
+    }
+
+    public String getStrMealThumb() {
+        return strMealThumb;
+    }
+
+    public String getIdMeal() {
+        return idMeal;
+    }
+
+    // Setter Methods
+
+    public void setStrMeal(String strMeal) {
+        this.strMeal = strMeal;
+    }
+
+    public void setStrMealThumb(String strMealThumb) {
+        this.strMealThumb = strMealThumb;
+    }
+
+    public void setIdMeal(String idMeal) {
+        this.idMeal = idMeal;
+    }
+}
---
+++ app/src/main/java/com/aalaa/foodplanner/data/model/FilterMealResponse.java
@@ -0,0 +1,7 @@
+package com.aalaa.foodplanner.data.model;
+
+import java.util.ArrayList;
+
+public class FilterMealResponse {
+    ArrayList<FilterMeal> meals = new ArrayList <FilterMeal> ();
+}
