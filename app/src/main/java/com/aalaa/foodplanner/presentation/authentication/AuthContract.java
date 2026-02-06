5829eb980c27e6319b6dfccbfebdd12f5632ac73
Aalaa Adel <aalaaadel02@gmail.com>
2026-02-06 07:14:08 +0200

    Add LoginPresenter and AuthContract for authentication logic separation

---
+++ app/src/main/java/com/aalaa/foodplanner/presentation/authentication/AuthContract.java
@@ -0,0 +1,35 @@
+package com.aalaa.foodplanner.presentation.authentication;
+
+import com.aalaa.foodplanner.presentation.base.BaseView;
+
+public interface AuthContract {
+
+    interface LoginView extends BaseView {
+        void showEmailError(String error);
+        void showPasswordError(String error);
+        void clearErrors();
+        void onLoginSuccess();
+    }
+
+    interface LoginPresenter {
+        void attachView(LoginView view);
+        void detachView();
+        void login(String email, String password);
+        void loginWithGoogle(String idToken);
+    }
+
+    interface SignUpView extends BaseView {
+        void showEmailError(String error);
+        void showPasswordError(String error);
+        void showConfirmPasswordError(String error);
+        void clearErrors();
+        void onSignUpSuccess();
+    }
+
+    interface SignUpPresenter {
+        void attachView(SignUpView view);
+        void detachView();
+        void signUp(String email, String password, String confirmPassword);
+        void signUpWithGoogle(String idToken);
+    }
+}
